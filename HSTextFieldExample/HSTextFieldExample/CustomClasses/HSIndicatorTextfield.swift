7550d958bfc4e9fb375a1378614c81be40516be4
Hasan Sedaghat <h.s_09@live.com>
2019-10-27 16:05:37 +0330

    no message

--- HSTextFieldExample/HSTextFieldExample/CustomClasses/HSIndicatorTextfield.swift
+++ HSTextFieldExample/HSTextFieldExample/CustomClasses/HSIndicatorTextfield.swift
@@ -11,9 +11,6 @@ import UIKit
 
 class HSIndicatorTextfield: UITextField {
     
-    private let gradientLayer = CAGradientLayer()
-    var IndicatorView = UIView()
-    
     @IBInspectable var indicatorHeight : CGFloat  {
         get {
             return self.IndicatorView.frame.height
@@ -23,7 +20,6 @@ class HSIndicatorTextfield: UITextField {
         }
     }
 
-    
     @IBInspectable var indicatorColor : UIColor  {
         get {
             return IndicatorView.backgroundColor!
@@ -33,86 +29,54 @@ class HSIndicatorTextfield: UITextField {
         }
     }
     
-    @IBInspectable var selectedIndicatorFirstColor : UIColor  {
-        get {
-            return self.firstColor
-        }
-        set {
-            self.firstColor = newValue
-        }
-    }
+    @IBInspectable var selectedIndicatorFirstColor : UIColor = .clear
+    @IBInspectable var selectedIndicatorSecondColor : UIColor = .clear
+    @IBInspectable var placeholderText : String = ""
+    @IBInspectable var placeholderTextColor : UIColor = .clear
+    @IBInspectable var placeholderFontSize : CGFloat = 15
+    @IBInspectable var unselectedTextColor : UIColor = .white
     
-    @IBInspectable var selectedIndicatorSecondColor : UIColor  {
-        get {
-            return self.secondColor
-        }
-        set {
-            self.secondColor = newValue
-        }
-    }
-    
-    @IBInspectable var placeholderText : String  {
-        get {
-            return placeholderString ?? ""
-        }
-        set {
-            self.placeholderString = newValue
-        }
-    }
-    
-    @IBInspectable var placeholderTextColor : UIColor  {
-        get {
-            return placeholderColor
-        }
-        set {
-            placeholderColor = newValue
-        }
-    }
-    
-    var firstColor = #colorLiteral(red: 0, green: 0.342538049, blue: 1, alpha: 1)
-    var secondColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
-    var placeholderString:String?
-    var placeholderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-    var placeholderFont = UIFont.systemFont(ofSize: 15)
+    private var IndicatorView = UIView()
+    private let gradientLayer = CAGradientLayer()
+    private var selectedColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
     
     override func awakeFromNib() {
-        
         self.borderStyle = .none
         IndicatorView.frame.size = CGSize(width: self.frame.size.width, height: indicatorHeight)
         IndicatorView.frame.origin = CGPoint(x: 0, y: self.frame.size.height - self.IndicatorView.frame.size.height)
         IndicatorView.backgroundColor = indicatorColor
-        self.placeholderFont = self.font!
-        self.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSAttributedString.Key.foregroundColor : placeholderColor,NSAttributedString.Key.font: placeholderFont])
-        
+        let placeholderFont = UIFont(name: self.font!.fontName, size: placeholderFontSize) ?? UIFont.systemFont(ofSize: placeholderFontSize)
+        self.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSAttributedString.Key.foregroundColor : placeholderTextColor,NSAttributedString.Key.font: placeholderFont])
+        selectedColor = self.textColor! 
         self.addTarget(self, action: #selector(textFieldDidBeginEditing(_:)), for: .editingDidBegin)
         self.addTarget(self, action: #selector(textFieldDidEndEditing(_:)), for: .editingDidEnd)
         
         self.assignSelectedColor()
-        
         self.addSubview(IndicatorView)
     
     }
     
-    func assignSelectedColor() {
+    private func assignSelectedColor() {
         self.gradientLayer.frame.size = CGSize(width: 0, height: self.IndicatorView.frame.size.height)
         self.gradientLayer.frame.origin = CGPoint(x: self.IndicatorView.frame.size.width / 2, y: self.IndicatorView.bounds.origin.y)
         gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
         gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-        gradientLayer.colors = [firstColor.cgColor, secondColor.cgColor]
+        gradientLayer.colors = [self.selectedIndicatorFirstColor.cgColor, selectedIndicatorSecondColor.cgColor]
         self.IndicatorView.layer.addSublayer(self.gradientLayer)
     }
     
-    @objc func textFieldDidBeginEditing(_ textField: UITextField) {
+    @objc private func textFieldDidBeginEditing(_ textField: UITextField) {
         UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
-            self.gradientLayer.frame.origin =  self.IndicatorView.bounds.origin
-            self.gradientLayer.frame.size = self.IndicatorView.frame.size
+            self.gradientLayer.frame =  self.IndicatorView.bounds
+            self.textColor = self.selectedColor
         }, completion: nil)
     }
     
-    @objc func textFieldDidEndEditing(_ textField: UITextField) {
+    @objc private func textFieldDidEndEditing(_ textField: UITextField) {
         UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
             self.gradientLayer.frame.size = CGSize(width: 0, height: self.IndicatorView.frame.size.height)
             self.gradientLayer.frame.origin = CGPoint(x: self.IndicatorView.frame.size.width / 2, y: self.IndicatorView.bounds.origin.y)
+            self.textColor = self.unselectedTextColor
         }, completion: nil)
     }
 
